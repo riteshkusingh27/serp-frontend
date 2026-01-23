630af865cec754ef588d0401e791d0d3bf707490
riteshkusingh <ritesh@gmail.com>
2026-01-23 13:57:26 +0530

    carousel edit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import favicon from './assets/favicon.png';
+
+const faviconLink = document.querySelector('link[rel="icon"]');
+if (faviconLink) {
+  faviconLink.href = favicon;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
